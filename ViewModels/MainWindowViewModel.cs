bfa370ffc6ce4fab8f607e91efcc6deeec770f0e
Zero <shinyzero0@gmail.com>
2023-02-15 20:11:50 +0300

    Не работает ObserveOnDispatcher

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,39 @@
-﻿namespace actualToDo.ViewModels;
+﻿using ReactiveUI;
+using System.Collections.ObjectModel;
+using Avalonia.ReactiveUI;
+using DynamicData;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 
-public class MainWindowViewModel : ViewModelBase
+namespace actualToDo.ViewModels;
+
+public class MainWindowViewModel : ReactiveObject, IActivatableViewModel
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public MainWindowViewModel()
+    {
+        Activator = new ViewModelActivator();
+        this.WhenActivated((CompositeDisposable disposables) =>
+        {
+            Disposable.Create(() => { })
+                      .DisposeWith(disposables);
+        });
+        _sourceList.Connect()
+                   .ObserveOnDispatcher()
+                   .Bind(out _list)
+                   .DisposeMany()
+                   .Subscribe();
+
+        _sourceList.Add(_greeting);
+        Greeting = "ну привет";
+    }
+    private string _greeting;
+    public string Greeting 
+    { 
+        get => _greeting;
+        set { this.RaiseAndSetIfChanged(ref _greeting, value); }
+    }
+    public ViewModelActivator Activator { get; }
+    private SourceList<string> _sourceList;
+    public ReadOnlyObservableCollection<string> List => _list;
+    private readonly ReadOnlyObservableCollection<string> _list;
 }

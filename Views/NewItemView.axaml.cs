49d57e7feb02004c9d23285606c499389378b944
Zero <shinyzero0@gmail.com>
2023-02-18 14:42:06 +0300

    Бинд кнопок к NotNull, автофокус текстбокса

--- Views/NewItemView.axaml.cs
+++ Views/NewItemView.axaml.cs
@@ -1,4 +1,5 @@
 using System;
+using Avalonia.Controls;
 using actualToDo.ViewModels;
 using Avalonia.ReactiveUI;
 using ReactiveUI;
@@ -10,6 +11,11 @@ public partial class NewItemView : ReactiveWindow<NewItemViewModel>
     public NewItemView()
     {
         InitializeComponent();
+        var InputBox = this.FindControl<TextBox>("InputBox");
+        if (InputBox is not null)
+        {
+            InputBox.AttachedToVisualTree += (s, e) => InputBox.Focus();
+        }
         this.WhenActivated(
             d => d(ViewModel.AcceptNewItemCommand.Subscribe(result => Close(result)))
         );

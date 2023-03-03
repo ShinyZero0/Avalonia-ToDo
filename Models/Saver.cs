35a3fbd81cc22d5cb4c93e7441a8f1fda5b73ae0
Zero <shinyzero0@gmail.com>
2023-03-03 20:38:00 +0300

    add environment path and null selections check for edit button

--- Models/Saver.cs
+++ Models/Saver.cs
@@ -1,27 +1,39 @@
 using Newtonsoft.Json;
+using System;
 using System.IO;
 
 namespace ToDo.Models;
 
 public class Saver
 {
+    private static string _data =>
+        Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "AvaloniaToDo.json"
+        );
+
     public static void Save(DataBase dataBase)
     {
-        using (var jsonSW = new StreamWriter(@"data.json"))
+        using (var jsonSW = new StreamWriter(_data))
         {
             jsonSW.Write(JsonConvert.SerializeObject(dataBase, Formatting.Indented));
         }
     }
+
     public static DataBase Get()
     {
         DataBase dataBase = new DataBase();
-        if (File.Exists(@"data.json"))
+        if (File.Exists(_data))
         {
-            using (var jsonSR = new StreamReader(@"data.json"))
+            using (var jsonSR = new StreamReader(_data))
             {
                 string jsonstr = jsonSR.ReadToEnd();
-                if (!string.IsNullOrWhiteSpace(jsonstr) && jsonstr.StartsWith("{") && jsonstr.EndsWith("}"))
-                { 
+                if (
+                    !string.IsNullOrWhiteSpace(jsonstr)
+                    && jsonstr.StartsWith("{")
+                    && jsonstr.EndsWith("}")
+                )
+                {
                     dataBase = JsonConvert.DeserializeObject<DataBase>(jsonstr);
                 }
             }

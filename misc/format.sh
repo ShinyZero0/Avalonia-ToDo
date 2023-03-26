fb9f3903bee92ae222f11b5ed606f84b294ea582
Zero <shinyzero0@gmail.com>
2023-03-27 00:37:47 +0300

    formatter update

--- misc/format.sh
+++ misc/format.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 scrpit_dir=$(dirname ${BASH_SOURCE[0]})
-fd "axaml$" $scrpit_dir/../ | xargs xstyler -c $scrpit_dir/format.json -i --indent-size 2 -f -
+{ fd "\.a?xa?ml$" $scrpit_dir/../ ; fd "\.csproj$" $scrpit_dir/../ ; } | xargs xstyler -c $scrpit_dir/format.json -i --indent-size 2 -f -
